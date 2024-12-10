ac99c60e88977d2f827cdc5a3887717aeb996fcc
Jagger-Guy <jaggertheguy@gmail.com>
2024-12-10 12:48:25 +0000

    a

---
+++ csproject/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+
+<div align="center">
+    <a href="/account/registration.php">Create an account</a><br>
+    <a href="/account/login.php">Log in</a>
+</div>
+
+<?php
+
+echo "hi";
+
+?>
+
+</body>
+</html>
